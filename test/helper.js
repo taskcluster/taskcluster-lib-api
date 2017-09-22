c86d0c5154134bb612b1e89452fe8b741d511422
ydidwania <didwaniayashvardhan+github@gmail.com>
2017-09-23 00:42:16 +0530

    Bug 1338047 - Migrated test/helper.js

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,13 @@
-var testing         = require('taskcluster-lib-testing');
-var validate        = require('taskcluster-lib-validate');
-var assert          = require('assert');
-var path            = require('path');
-var express         = require('express');
+import testing from 'taskcluster-lib-testing';
+import validate from 'taskcluster-lib-validate';
+import assert from 'assert';
+import path from 'path';
+import express from 'express';
+// var testing         = require('taskcluster-lib-testing');
+// var validate        = require('taskcluster-lib-validate');
+// var assert          = require('assert');
+// var path            = require('path');
+// var express         = require('express');
 
 var runningServer = null;
 
